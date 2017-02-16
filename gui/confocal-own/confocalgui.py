343079eaee1c30373d0e346b4ca3319792796a5a
olgob <olgob@fysik.dtu.dk>
2017-02-16 10:19:49 +0100

    Including GUI files

---
+++ gui/confocal-own/confocalgui.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+
+"""
+This file contains the Qudi GUI for general Confocal control.
+
+Qudi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Qudi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Qudi. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (c) the Qudi Developers. See the COPYRIGHT.txt file at the
+top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi/>
+"""
+
+from qtpy import QtCore
+from qtpy import QtGui
+from qtpy import QtWidgets
+from qtpy import uic
+import pyqtgraph as pg
+import numpy as np
+import time
+import os
+
+from gui.guibase import GUIBase
+
+class ConfocalMainWindow(QtWidgets.QMainWindow):
+
+    """ Create the Mainwindow based on the corresponding *.ui file. """
+        
+    def __init__(self, **kwargs):
+        # Get the path to the *.ui file
+        this_dir = os.path.dirname(__file__)
+        ui_file = os.path.join(this_dir, 'attocubePositionerGUI.ui')
+
+        # Load it
+        super().__init__(**kwargs)
+        uic.loadUi(ui_file, self)
+        self.show()
+
+class ConfocalGui(GUIBase):
+
+    """ Main Confocal Class for xy and depth scans.
+    """
+    _modclass = 'ConfocalGui'
+    _modtype = 'gui'
+
+    # declare connectors
+    _in = {'confocallogic1': 'ConfocalLogic',
+           'savelogic': 'SaveLogic',
+           }
+
+    sigStartOptimizer = QtCore.Signal(list, str)
+
+    def __init__(self, config, **kwargs):
+        super().__init__(config=config, **kwargs)
+
+        self.log.info('The following configuration was found.')
+
+        # checking for the right configuration
+        for key in config.keys():
+            self.log.info('{0}: {1}'.format(key, config[key]))
+
+
+
+    def on_activate(self, e=None):
+        """ Initializes all needed UI files and establishes the connectors.
+
+        @param object e: Fysom.event object from Fysom class.
+                         An object created by the state machine module Fysom,
+                         which is connected to a specific event (have a look in
+                         the Base Class). This object contains the passed event,
+                         the state before the event happened and the destination
+                         of the state which should be reached after the event
+                         had happened.
+
+        This method executes the all the inits for the differnt GUIs and passes
+        the event argument from fysom to the methods.
+        """
+
+        # Getting an access to all connectors:
+        self._scanning_logic = self.get_in_connector('confocallogic1')
+        self._save_logic = self.get_in_connector('savelogic')
+        
+        self._mw = ConfocalMainWindow()
+                
+
+    def on_deactivate(self, e):
+        """ Reverse steps of activation
+
+        @param object e: Fysom.event object from Fysom class. A more detailed
+                         explanation can be found in the method initUI.
+
+        @return int: error code (0:OK, -1:error)
+        """
+        self._mw.close()
+        return 0
+
+    def show(self):
+        """Make main window visible and put it above all other windows. """
+        # Show the Main Confocal GUI:
+        self._mw.show()
+        self._mw.activateWindow()
+        self._mw.raise_()
+
+    
